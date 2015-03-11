f54118222d6c3db1a51a14ab1fdf73437cd3f93f
Omar Medina <omarhack13@gmail.com>
2015-03-11 13:34:40 -0600

    Commit Preparing to add auth

--- app/controllers/users_Controller.php
+++ app/controllers/users_Controller.php
@@ -1,7 +1,8 @@
 <?php
 class UsersController extends AppController{
 	var $layout = 'login';
-	
+	var $useDbConfig = 'admin';
+
 	function index(){
 	//action logic goes here
 	}
@@ -11,5 +12,15 @@ class UsersController extends AppController{
 	function logout(){
 
 	}
+	var $components = array( 'Auth', 'Session');
+    var $helpers = array('Html', 'Form', 'Session');
+
+    function beforeFilter() {
+        //Configure AuthComponent
+        $this->Auth->authorize = 'actions';
+        $this->Auth->loginAction = array('controller' => 'users', 'action' => 'login');
+        $this->Auth->logoutRedirect = array('controller' => 'users', 'action' => 'logout');
+        $this->Auth->loginRedirect = array('controller' => 'posts', 'action' => 'add');
+    }
 }
 ?>
